d2e9d16a4e6ccc442209d7f2de6cc599492f4b0a
lucky521 <307479836@qq.com>
2016-04-14 21:28:54 +0800

    rename

--- java_md/WordCount.java
+++ java_md/WordCount.java
@@ -15,15 +15,17 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 public class WordCount {
         
  public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
-    private final static IntWritable one = new IntWritable(1);
+
+    private final static IntWritable one = new IntWritable(1); // init value is 1
     private Text word = new Text();
         
-    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+    public void map(LongWritable key, Text value, Context context) 
+      throws IOException, InterruptedException {
         String line = value.toString();
         StringTokenizer tokenizer = new StringTokenizer(line);
         while (tokenizer.hasMoreTokens()) {
             word.set(tokenizer.nextToken());
-            context.write(word, one);
+            context.write(word, one); // generate a output key/value pair
         }
     }
  } 
@@ -34,26 +36,30 @@ public class WordCount {
       throws IOException, InterruptedException {
         int sum = 0;
         for (IntWritable val : values) {
-            sum += val.get();
+            sum += val.get(); // merge values of same key
         }
-        context.write(key, new IntWritable(sum));
+        context.write(key, new IntWritable(sum)); // generate a output key/value pair
     }
  }
         
  public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
         
-        Job job = new Job(conf, "wordcount");
-    
+    Job job = new Job(conf, "wordcount");
+   
+    // set type of key,value 
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(IntWritable.class);
-        
+    
+    // set map and reduce class that I write 
     job.setMapperClass(Map.class);
     job.setReducerClass(Reduce.class);
-        
+    
+    // set format of input and output data
     job.setInputFormatClass(TextInputFormat.class);
     job.setOutputFormatClass(TextOutputFormat.class);
-        
+
+    // get file path from args        
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
         
